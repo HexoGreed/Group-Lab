ae77d2735d4f2a561d79c3a4cfa83b20afc27548
Romeo Shamoun <170680705+7onyx7@users.noreply.github.com>
2025-04-24 18:14:23 -0500

    Display Board successfully

---
+++ README.md
@@ -0,0 +1,4 @@
+# Group-Lab
+This is for the group lab.
+Link to the google doc https://docs.google.com/document/d/1s93DjlR810nbo-e14f8ctEWXjuG4Ibt0C6oiK0zIkJw/edit?usp=sharing
+
---
+++ board.h
@@ -0,0 +1,26 @@
+#pragma once
+
+struct Cell {
+	int row;
+	int col;
+	char value;
+	bool isMine;
+	bool isRevealed;
+	bool isFlagged;
+
+};
+
+class Board {
+public:
+	Board();
+	~Board();
+	void initialize(int rows, int cols, int mines);
+	void printBoard();
+
+private:
+	int rows;
+	int cols;
+	int mines;
+	Cell** cells;
+
+};
